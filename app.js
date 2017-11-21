76e1d62a2180e984c97b8cbdd55df20c092b599c
Tomáš Klapka <tomas@klapka.cz>
2017-11-21 23:45:28 +0100

    enabled CORS

--- app.js
+++ app.js
@@ -28,11 +28,16 @@ app.set('port', process.env.PORT || 3042);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 app.enable('trust proxy');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(favicon(join(__dirname, 'public/favicon.ico')));
 app.use(logger('dev'));
 app.set('json spaces', 2);
 app.use(express.static(join(__dirname, 'public')));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', routes.index);
 app.get('/users', user.list);
